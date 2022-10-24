a4be16b5be0477527e8eaa83944ccfad93162ce0
soft <sandro.brito@gft.com>
2022-10-24 13:28:13 -0300

    AdicionandoAulas

---
+++ Javascript/javascript/assets/js/3.EventosManipulaçãoDom/1.2.OqueEDom.js
@@ -0,0 +1,14 @@
+/*DOM é a sigla em inglês para Document Object Model, 
+que significa modelo de documento por objetos. 
+Apesar de se tratar de um assunto complexo, 
+ele já está embutido dentro dos navegadores. 
+O que significa que a parte em que os programadores 
+têm acesso é bem mais simples e fácil de manipular.
+
+É uma interface padronizada que permite que navegadores e 
+scripts possam manipular o conteúdo de uma página web sem 
+a necessidade de realizar atualizações e upgrades. É ele, 
+ainda, que põe ordem na navegação web para que o conteúdo 
+fique acessível a todos.
+
+*/
---
+++ Javascript/javascript/assets/js/3.EventosManipulaçãoDom/3.SelecionandoElementos.js
@@ -0,0 +1,23 @@
+//As principais formas de pegar um elemento
+
+// sempre o Elements estiver no plural retornara um array
+
+//document.getElementById("teste") = selecionando tag por Id
+
+//document.getElementsBytagName("h1") = selecionando uma tag por nome no documento
+
+//document.getElementsByClassName("botao") selecionando por classe
+
+//document.getElementsByName(""); seleciona um elemento por name
+
+//querySelector(); pega qualquer elemento especificado pelo paramentro
+//Exemplo abaixo
+
+//document.queySelector("#teste"); pegando pelo Id
+//document.querySelector(".botao"); pegando pela class
+//document.querySelector("h1"); pegando pelo elemento
+
+//querySelectorAll retorna sempre um array
+//document.querySelectorAll("li"); pegando todos itens do elemento
+//nesse exemplo de uma lista
+
